18cc2a0a0dc32557c55a3f718a5a46033503999d
BruceJi7 <revolverc24@gmail.com>
2019-12-19 19:35:01 +0900

    create new better version of CLI hangman

--- hangman.py
+++ hangman.py
@@ -1,56 +1,102 @@
-import random
-
-wordList = ['telescope', 'orange', 'toy car']
-wordToGuess = wordList[2]
-
-hiddenWordList = []
-for letter in wordToGuess:
-    if letter.isalpha():
-        hiddenWordList.append('_')
-    elif letter.isspace():
-        hiddenWordList.append(' ')
-
-# This part helps for displaying the word in this test CLI version.
-hiddenWordString = ' / '.join(hiddenWordList)    
-
-wordGuessed = False
-
-guessedLetters = []
+import logging, random
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.disable(logging.CRITICAL)
 
 
+wordsToPlayWith = ['horse', 'tractor', 'goat', 'chicken' ]
 
-def revealGuessedLetters(guessedLetters, wordToGuess):
-    partiallyRevealed = []
+# This prints the word with the leading and trailing ----s.
+def printHidden(word):
+    outWord = ' '.join(word)
+    print('-' * len(outWord))
+    print(outWord)
+    print('-' * len(outWord) + '\n')
     
- 
-    for hiddenLetter in wordToGuess:
-        if hiddenLetter in guessedLetters:
-            partiallyRevealed.append(hiddenLetter)
-        else:
-            if hiddenLetter.isspace():
-                partiallyRevealed.append(' ')
-            else:
-                partiallyRevealed.append('_')
-    return partiallyRevealed
+
+def startGame(chances):
+    targetWord = random.choice(wordsToPlayWith)
+    logging.debug(f'Word is {targetWord}\n')
+    
+    secretWord = [char.upper() for char in targetWord]
+    guessedLetters = []
         
-def getNextGuess(guessedLetters):
-    while True:
-        currentGuess = input('Guess a letter: ').lower()
-        if currentGuess not in guessedLetters:
-            break
-    guessedLetters.append(currentGuess)
-    return guessedLetters
+    return (secretWord, obfuscate(secretWord), chances, guessedLetters)
 
-def winCondition(progressWord, hiddenWord):
-    if '_' not in  progressWord:
-        return True
+
+def getGuess():
+    guess = input('Guess a letter:\n').upper()
+    logging.debug(f'Guess is {guess}\n')
+    return guess
+
+def obfuscate(inwordL):
+    secretWord = ['_' for char in inwordL]
+    if ' ' in inwordL:
+        for index, char in enumerate(inwordL):
+            if char == ' ':
+                secretWord[index] = '/'
+    return secretWord
+
+def revealLetter(guess, wordT):
+    targetWord = wordT[0]
+    hiddenWord = wordT[1]
+    chances = wordT[2]
+    guessedLetters = wordT[3]
+    
+    if guess in guessedLetters: # What if you enter a letter twice?
+        print('That letter has already been guessed')
+        printHidden(hiddenWord)
+        print(f'Letters guessed so far: {", ".join(guessedLetters)}')
+        return(targetWord, hiddenWord, chances, guessedLetters)
     else:
+        if guess in targetWord: # If your guess is correct
+            guessedLetters.append(guess) # Add guess to guessed letter
+            for index, char in enumerate(targetWord): # Handles replacing _ for the correct letter
+                if char == guess:
+                    hiddenWord[index] = targetWord[index]
+                    logging.debug(f'{hiddenWord} character {hiddenWord[index]} matched')
+            print(f'{guess} is in the word!')    
+            printHidden(hiddenWord)
+            print(f'Letters guessed so far: {", ".join(guessedLetters)}')
+            return(targetWord, hiddenWord, chances, guessedLetters)
+                
+        else:
+            chances -= 1 # If you are wrong, lose a chance.
+            guessedLetters.append(guess)
+            print(f'{guess} is not in the word\n')
+            printHidden(hiddenWord)
+            print(f'Letters guessed so far: {", ".join(guessedLetters)}')
+            return(targetWord, hiddenWord, chances, guessedLetters)
+
+def checkProgress(wordT):
+
+    hiddenWord = wordT[1]
+    
+    if '_' in hiddenWord:
         return False
+    else:
+        return True
 
+# Below is game code:
+ 
+print("Let's play hangman!\n")
+difficulty = 12       
+secretWordT = startGame(difficulty)
+print(f'Okay. You now have {difficulty} guesses to guess the word\n')
 
-while not wordGuessed:
-    progressWord = revealGuessedLetters(guessedLetters, wordToGuess)
-    print(' / '.join(progressWord))
-    guessedLetters = getNextGuess(guessedLetters)
-
-    wordGuessed = winCondition(progressWord, wordToGuess)
+remainingChances = difficulty
+while remainingChances > 0:
+    
+    guess = getGuess()
+    updatedWord = revealLetter(guess, secretWordT)
+    secretWordT = updatedWord
+    if checkProgress(updatedWord):
+        print('Word successfully guessed!')
+        break
+    else:
+        remainingChances = updatedWord[2]
+        if remainingChances == 1:
+            guessplu = 'guess'
+        else:
+            guessplu = 'guesses'
+        print(f'You have {remainingChances} {guessplu} left!')
+print(f"The game is over! The word was {''.join(secretWordT[0])}")   
